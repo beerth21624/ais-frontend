10fadf39652a1fc1057760bbce0c933e43f2af22
beerth21624 <beerzii.4321@gmail.com>
2024-06-30 14:18:35 +0700

    resize screen mobile

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const documentHeight = () => {
+  const doc = document.documentElement
+  doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
+}
+window.addEventListener('resize', documentHeight)
+documentHeight()
+
 root.render(
   <React.StrictMode>
     <App />
